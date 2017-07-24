4f1989f71afe14eca1e42621bdd092ed08289f5a
Nagaraj <raj.nagaraj1990@gmail.com>
2017-07-24 20:11:53 +0530

    changes

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,10 @@
 const ORDER_LUNCH = 'order.lunch';
 const SELECT_LUNCH = 'select.lunch';
 const ORDER_LUNCH_SELECT_CONFIRM = 'orderlunch.select.confirm';
+const TRANSACTION_CHECK_COMPLETE = 'transaction.check.complete';
+const TRANSACTION_DECISION_COMPLETE = 'transaction.decision.complete';
+const CART_CHANGE_REQUESTED ='CART_CHANGE_REQUESTED';
+const ADD_MORE_CONFIRM  ='add.more.confirm';
 
 const FOOD_SELECTED = 'thinking about lunch ah?';
 
@@ -61,10 +65,6 @@ const LUNCH_IMAGES = [
   ]
 ];
 
-function getRandomImage(images) {
-  let randomIndex = Math.floor(Math.random() * images.length);
-  return images[randomIndex];
-}
 
 function getLunchItem(key){
   var filtered = LUNCH_VALUES.filter(function(item){
@@ -73,17 +73,19 @@ function getLunchItem(key){
   return filtered;
 }
 
-//functions.https.onRequest()
-
-exports.embothook = (request, response) => {
+exports.embothook = functions.https.onRequest((request, response) => {
   const app = new App({ request, response });
+  app.data.foodItems=[];
   console.log('Request headers: ' + JSON.stringify(request.headers));
   console.log('Request body: ' + JSON.stringify(request.body));
-  app.setContext('order_lunch-followup');
-  function orderlunch(app) {
+  function orderlunch(app,addmore) {
+    let text='Alright what are you in the mood for?'
+    if(addmore){
+       text='All right!! which dish you would like to add'
+    }
     var lunchimage = LUNCH_IMAGES;
     app.askWithList(app.buildRichResponse()
-      .addSimpleResponse('Alright what are you in the mood for?'),
+      .addSimpleResponse(text),
       // Build a list
       app.buildList('Most popular choices')
         // Add the first item to the list
@@ -124,9 +126,120 @@ exports.embothook = (request, response) => {
 
   function selectlunch(app) {
     var quantity = app.getArgument('Quantity');
-    var Lunch = app.getArgument('Lunch');
-    app.ask('Gotcha!! '+ getLunchItem(Lunch)[0].value +' - '+quantity+' plates coming right away' );
+    var lunch = app.getArgument('Lunch');
+    var lunchItem = getLunchItem(lunch)[0];
+    var selectedLunch= lunchItem.value;
+    var selectedQuantity= quantity;
+    var selectedCost= quantity * lunchItem.cost;
+
+    app.data.foodItems.push({'selectedLunch':selectedLunch,
+    'selectedQuantity':selectedQuantity,'selectedCost':selectedCost});
+
+    app.askForConfirmation('add more items?');
+    //app.ask('Gotcha!! '+ getLunchItem(Lunch)[0].value +' - '+quantity+' plates coming right away' );
+    //app.askForTransactionRequirements();
   }
+
+  function transactionDecision (app) {
+    var buildItems=[];
+    var subtotal=0;
+    var tax=5;
+    app.data.foodItems.forEach(function(foodItem){
+      buildItems.push(app.buildLineItem(foodItem.selectedLunch, foodItem.selectedLunch)
+            .setPrice(app.Transactions.PriceType.ACTUAL, 'INR',foodItem.selectedCost)
+            .setQuantity(foodItem.selectedQuantity))
+            subtotal=subtotal+foodItem.selectedCost;
+    })
+    let order = app.buildOrder('321432')
+      .setCart(app.buildCart().setMerchant('Carnival FC', 'Carnival FC')
+        .addLineItems(buildItems).setNotes('Lunch Items'))
+      .addOtherItems([
+        app.buildLineItem('subtotal', 'Subtotal')
+          .setType(app.Transactions.ItemType.SUBTOTAL)
+          .setQuantity(1)
+          .setPrice(app.Transactions.PriceType.ESTIMATE, 'INR', subtotal),
+        app.buildLineItem('tax', 'Tax')
+          .setType(app.Transactions.ItemType.TAX)
+          .setQuantity(1)
+          .setPrice(app.Transactions.PriceType.ESTIMATE, 'INR', tax)
+      ])
+      .setTotalPrice(app.Transactions.PriceType.ESTIMATE, 'INR', (subtotal+tax));
+
+    // If in sandbox testing mode, do not require payment
+    if (app.isInSandbox()) {
+      app.askForTransactionDecision(order);
+    } else {
+      // To test this sample, uncheck the 'Testing in Sandbox Mode' box in the
+      // Actions console simulator
+      app.askForTransactionDecision(order, {
+        type: app.Transactions.PaymentType.PAYMENT_CARD,
+        displayName: 'VISA-1234',
+        deliveryAddressRequired: false
+      });
+
+      /*
+        // If using Google provided payment instrument instead
+        app.askForTransactionDecision(order, {
+          // These will be provided by payment processor, like Stripe,
+          // Braintree, or Vantiv
+          tokenizationParameters: {},
+          cardNetworks: [
+            app.Transactions.CardNetwork.VISA,
+            app.Transactions.CardNetwork.AMEX
+          ],
+          prepaidCardDisallowed: false,
+          deliveryAddressRequired: false
+        });
+      */
+    }
+  }
+
+  function transactionCheckComplete(app){
+    if (app.getTransactionRequirementsResult().resultType ===
+      app.Transactions.ResultType.ACCEPTED) {
+      // Normally take the user through cart building flow
+      transactionDecision(app);
+    } else {
+      app.tell('Transaction failed.');
+    }
+  }
+
+   function transactionDecisionComplete (app) {
+    if (app.getTransactionDecision() &&
+      app.getTransactionDecision().userDecision ===
+        app.Transactions.ConfirmationDecision.ACCEPTED) {
+      let googleOrderId = app.getTransactionDecision().order.googleOrderId;
+
+      // Confirm order and make any charges in order processing backend
+      // If using Google provided payment instrument:
+      // let paymentToken = app.getTransactionDecision().order.paymentInfo
+      //   .googleProvidedPaymentInstrument.instrumentToken;
+
+      app.tell(app.buildRichResponse().addOrderUpdate(
+        app.buildOrderUpdate(googleOrderId, true)
+          .setOrderState(app.Transactions.OrderState.CREATED, 'Order created')
+          .setInfo(app.Transactions.OrderStateInfo.RECEIPT, {
+            confirmedActionOrderId: '321432'
+          }))
+        .addSimpleResponse('Transaction completed! You\'re all set!'));
+    } else if (app.getTransactionDecision() &&
+      app.getTransactionDecision().userDecision ===
+        app.Transactions.ConfirmationDecision.CART_CHANGE_REQUESTED) {
+      return orderlunch(app,true);
+    } else {
+      app.tell('Transaction failed.');
+    }
+  }
+
+  function addMoreConfirm(app){
+    if (app.getUserConfirmation()) {
+      orderlunch(app,true)
+    } else {
+      app.askForTransactionRequirements();
+    }
+  }
+
+
 
 
 
@@ -135,6 +248,9 @@ exports.embothook = (request, response) => {
   actionMap.set(ORDER_LUNCH, orderlunch);
   actionMap.set(SELECT_LUNCH, selectlunch);
   actionMap.set(ORDER_LUNCH_SELECT_CONFIRM, selectlunch);
+  actionMap.set(TRANSACTION_CHECK_COMPLETE, transactionCheckComplete);
+  actionMap.set(TRANSACTION_DECISION_COMPLETE, transactionDecisionComplete);
+  actionMap.set(ADD_MORE_CONFIRM, addMoreConfirm);
   app.handleRequest(actionMap);
 
-};
+});
